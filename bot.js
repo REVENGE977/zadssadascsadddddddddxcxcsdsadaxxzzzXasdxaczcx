ba5a4711e7b2afe8821b5e1f491fb7f986e6582f
troolerrrrrrr <32039975+troolerrrrrrr@users.noreply.github.com>
2018-07-20 13:01:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,6 +83,50 @@ client.on('message', function(msg) {
 
 
 
+
+let rebel;
+client.on("ready", async  => {
+    let guild = client.guilds.get("414474375044268042");
+  let users = guild.members.map(member => member.user.id);
+  let i;
+  rebel=0;
+for (i=0 ; i < users.length ; i++) {
+ let   check = guild.members.get(users[i]);
+if(!check.voiceChannelID){
+        continue;
+}else{
+  rebel++;
+}
+}
+guild.channels.find('id', '469804486966116353').setName(" Voice Online「"+rebel+"」");
+  client.setInterval(() =>{
+    let d = Date.now()
+  }, 5000);
+});
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    let guild = client.guilds.get("414474375044268042");
+let newUserChannel = newMember.voiceChannel
+let oldUserChannel = oldMember.voiceChannel
+ if(oldUserChannel === undefined && newUserChannel !== undefined) {
+   rebel++;
+guild.channels.find('id', '469804486966116353').setName(" Voice Online「"+rebel+"」");
+} else if(newUserChannel === undefined){
+  rebel--;
+guild.channels.find('id', '469804486966116353').setName(" Voice Online「"+rebel+"」");
+}
+});
+client.on('message', Codes => {
+  
+  if(Codes.content === "$صوت") {
+      Codes.channel.send(" Voice「"+rebel+"」");
+}
+});
+
+
+
+
+
+
 client.on('message', message => {
    if(!message.channel.guild) return;
 if(message.content.startsWith(prefix + 'clear')) {
