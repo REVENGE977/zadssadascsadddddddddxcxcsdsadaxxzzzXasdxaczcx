f2ba73ea5cf5f364cad98cc311e18f04f6d20827
troolerrrrrrr <32039975+troolerrrrrrr@users.noreply.github.com>
2018-07-21 01:10:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,39 +29,6 @@ client.on('ready', () => {
 });
 
 
-client.on('message', message => {
-  if(message.channel.type === "dm") return;
-    if(message.content.startsWith ("$زواج")) {
-    if(!message.channel.guild) return message.reply(' This command only for servers ')
-    var proposed = message.mentions.members.first()
-
-    if(!message.mentions.members.first()) return message.reply('لازم تطلب ايد وحدة').catch(console.error);
-    if(message.mentions.users.size > 1) return message.reply('ولد ما يضبط لازم بنت تذكر لازم بنت الحلال').catch(console.error);
-      if(proposed === client.user) return message.reply(`** تبي تتزوجني؟ **`)
-            message.channel.send(`**${proposed} 
-بدك تقبلي عرض الزواج من ${message.author}
-العرض لمدة 10 ثانية 
-اكتب موافقة او لا**`)
-
-const filter = m => m.content.startsWith("موافقة");
-message.channel.awaitMessages(filter, { max: 1, time: 15000, errors: ['time'] })
-.then(collected =>{ 
-  message.channel.send(`**${message.author} و ${proposed} الف الف مبروك انشاء الله تستمتعون بحياتكم الزوجية ويطول اعماركم ولا تنسون شهر العسل**`);
-})
- .catch(collected => message.channel.send(`**السكوت علامة الرضا نقول قلللوش مبروك**`))
-
- const filte = m => m.content.startsWith("لا");
-message.channel.awaitMessages(filte, { max: 1, time: 15000, errors: ['time'] })
-.then(collected =>{ 
- message.channel.send(`**${message.author} تم رفض عرضك**`);
-})
-
-
-
-
-}
-});
-
 
 
 client.on('message', message => {
@@ -4096,6 +4063,39 @@ client.on("guildMemberRemove", function(member) {
 
 
 
+
+client.on('message', message => {
+  if(message.channel.type === "dm") return;
+    if(message.content.startsWith ("$زواج")) {
+    if(!message.channel.guild) return message.reply(' This command only for servers ')
+    var proposed = message.mentions.members.first()
+
+    if(!message.mentions.members.first()) return message.reply('لازم تطلب ايد وحدة').catch(console.error);
+    if(message.mentions.users.size > 1) return message.reply('ولد ما يضبط لازم بنت تذكر لازم بنت الحلال').catch(console.error);
+      if(proposed === client.user) return message.reply(`** تبي تتزوجني؟ **`)
+            message.channel.send(`**${proposed} 
+بدك تقبلي عرض الزواج من ${message.author}
+العرض لمدة 10 ثانية 
+اكتب موافقة او لا**`)
+
+const filter = m => m.content.startsWith("موافقة");
+message.channel.awaitMessages(filter, { max: 1, time: 15000, errors: ['time'] })
+.then(collected =>{ 
+  message.channel.send(`**${message.author} و ${proposed} الف الف مبروك انشاء الله تستمتعون بحياتكم الزوجية ويطول اعماركم ولا تنسون شهر العسل**`);
+})
+ .catch(collected => message.channel.send(`**السكوت علامة الرضا نقول قلللوش مبروك**`))
+
+ const filte = m => m.content.startsWith("لا");
+message.channel.awaitMessages(filte, { max: 1, time: 15000, errors: ['time'] })
+.then(collected =>{ 
+ message.channel.send(`**${message.author} تم رفض عرضك**`);
+})
+
+
+
+
+}
+});
 
 
 
