e61f0e30d6a3940457adf20988ca15d1b2ed284d
troolerrrrrrr <32039975+troolerrrrrrr@users.noreply.github.com>
2018-08-01 19:33:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4048,12 +4048,12 @@ client.on('ready', () => {
 
 
 });
-let points = JSON.parse(fs.readFileSync('./typePTS.json', 'utf8')); // يقوم بقراءه ملف النقاط , والمسار حق النقاط
+let pointts = JSON.parse(fs.readFileSync('./typePTS.json', 'utf8')); // يقوم بقراءه ملف النقاط , والمسار حق النقاط
 
 
 client.on('message', message => {
-if (!points[message.author.id]) points[message.author.id] = { // يقوم الكود تلقائياً في حال لم يجد نقاط العضو بإنشاء نقاط له ويتم إرسالها الملف المخصص
-	points: 0,
+if (!pointts[message.author.id]) pointts[message.author.id] = { // يقوم الكود تلقائياً في حال لم يجد نقاط العضو بإنشاء نقاط له ويتم إرسالها الملف المخصص
+	pointts: 0,
   };
 if (message.content.startsWith(prefix + 'سرعة')) { // .سرعة
 	if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
@@ -4075,7 +4075,7 @@ message.channel.send('**لديك 15 ثانية لكتابة الكلمة**').the
 		message.channel.send(`${collected.first().author} ✅ **لقد قمت بكتابة الكلمة بالوقت المناسب**`);
 		console.log(`[Typing] ${collected.first().author} typed the word.`);
             let won = collected.first().author; // في هذا السطر يقوم الكود بسحب الأي دي الذي قام بالأجابة اولاً
-            points[won.id].points++;
+            pointtts[won.id].pointts++;
           })
           .catch(collected => { // في حال لم يقم أحد بالإجابة
             message.channel.send(`:x: **لم يقم أحد بكتابة الجملة بالوقت المناسب**`);
@@ -4087,18 +4087,18 @@ message.channel.send('**لديك 15 ثانية لكتابة الكلمة**').the
 });
 if (message.content.startsWith(prefix + 'نقاطي')) {
 	if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
-	let userData = points[message.author.id];
+	let userData = pointts[message.author.id];
 	let embed = new Discord.RichEmbed()
     .setAuthor(`${message.author.tag}`, message.author.avatarURL)
 	.setColor('#000000')
 	.setFooter("بوت سرعة الكتابة", '')
-	.setDescription(`نقاطك: \`${userData.points}\``)
+	.setDescription(`نقاطك: \`${userData.pointts}\``)
 	message.channel.sendEmbed(embed)
   }
-  fs.writeFile("./typePTS.json", JSON.stringify(points), (err) => {
+  fs.writeFile("./typePTS.json", JSON.stringify(pointts), (err) => {
     if (err) console.error(err)
   })
-});
+
 client.on('guildCreate', guild => {
 	console.log(`Added to a server by: ${guild.owner.user.username} || Server name: ${guild.name} || Users: ${guild.memberCount}`); // ايفنت يقوم بإرسال إلى الكونسل بأنه قد قامت احد السيرفر بدعوة البوت
 });
