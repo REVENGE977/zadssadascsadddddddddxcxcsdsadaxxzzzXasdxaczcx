8f7e7dcead23990eac9cc6751adb23cce6a72ee9
ShadowX <32039975+ShadwX57@users.noreply.github.com>
2018-08-05 21:26:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3919,7 +3919,8 @@ if (!rank) return message.reply('انت لا تمتلك الرتبه المخص�
 
 client.on('message', message => {
 client.on('voiceStateUpdate', (old, now) => {
-    const channel = client.channels.get('472417985047560193')
+    const channel = client.channels.get('475730153201336320')
+    const allplayer = message.guild.memberCount
     const size = channel.name.match(/\[\s(\d+)\s\]/);
     if (!size) return channel.setName(`Users: [ ${message.guild.memberCount} ]`);
     if (allplayer !== size) channel.setName(`Users: [ ${message.guild.memberCount} ]`);
